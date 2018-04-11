9e510eb3aaffecedbed6ea431c2e90b931539e20
Heindorf <Jacob.Heindorf@haw-hamburg.de>
2018-04-11 11:03:43 +0200

    Hallo

---
+++ bit/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.engine('.ejs', require('ejs').__express);
+app.set('view engine', 'ejs');
+
+const port = 3000;
+app.listen(port, function() {
+	console.log('Affirmative');
+});
+
+
